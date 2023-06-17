a25950ba5451d58901492693a0570c937b89d2e3
radcode-gh <radiyahilyas3@gmail.com>
2023-06-17 01:25:41 +0000

    added new section

---
+++ index.js
@@ -0,0 +1,27 @@
+// function initMap() {
+//     var mapOptions = {
+//       center: { lat: [7.9465, 11.1734] lng: [1.1915, 3.2559] },
+//       zoom: 10
+//     };
+//     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//   }
+
+function initMap() {
+    var mapOptions = {
+      center: { lat: 7.9465, lng: 3.2559 },
+      zoom: 10
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  }
+
+  
+
+
+
+
+
+
+
+  initMap();
+
+  
